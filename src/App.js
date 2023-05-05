620702aac5ceee9f1a07635b78c19140169a1bb3
Anton <borzenkov0anton@gmail.com>
2023-05-05 16:31:04 +0200

    setuped proj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Caclulator
       </header>
+      <form className='form'>
+        <input className='textview' type = 'text'></input>
+        <table className='table'>
+          <tr>
+            <td> <input type = "button" className='button' value={'C'}></input></td>
+            <td> <input type = "button" className='button' value={'<'}></input></td>
+            <td> <input type = "button" className='button' value={'/'}></input></td>
+            <td> <input type = "button" className='button' value={'X'}></input></td>
+          </tr>
+          <tr>
+            <td> <input type = "button" className='button' value={7}></input></td>
+            <td> <input type = "button" className='button' Value={8}></input></td>
+            <td> <input type = "button" className='button' value={9}></input></td>
+            <td> <input type = "button" className='button' value={'-'}></input></td>
+          </tr>
+          <tr>
+            <td> <input type = "button" className='button' value={4}></input></td>
+            <td> <input type = "button" className='button' value={5}></input></td>
+            <td> <input type = "button" className='button' value={6}></input></td>
+            <td> <input type = "button" className='button' value={'+'}></input></td>
+          </tr>
+          <tr>
+            <td> <input type = "button" className='button' value={1}></input></td>
+            <td> <input type = "button" className='button' value={2}></input></td>
+            <td> <input type = "button" className='button' value={3}></input></td>
+            <td> <input type = "button" className='button' value={'='}></input></td>
+          </tr>
+        </table>
+      </form>
     </div>
   );
 }
